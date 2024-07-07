dd9f240df56a0e088920eaff7d83a4b76ec3bf7f
Proush <proushbir@gmail.com>
2024-07-07 16:34:15 +0545

    code revised for day 1

---
+++ Basci of python/day1.py
@@ -0,0 +1,57 @@
+print("hello World")
+
+a=10
+b=5
+print(a)
+
+#define user variable called as name
+name="QA class"
+print(name)
+print("Name is:",name)
+
+print("firstline\n\n")
+print("second linae")
+
+#operators
+#operators is a symbol to carry out arthemantical calculations
+#or logical operations
+
+a=5
+b=3
+c=1
+sum = a + b -c
+print("sum is: ",sum)
+#arthematic operators
+# +,-,*,/,%
+
+
+#concatinations of strings
+first_name="test"
+last_name="test2"
+name=first_name + " " + last_name
+print(name)
+
+#-->*
+result=5*2
+print("result is:", result)
+
+#-->* can be used in the repeatation of string
+a="pyhton_automation"
+b=a*3
+print(b)
+# but it cannot be used in string value like:
+# a="abc"
+# b=a*"3"
+# print(b)
+
+
+#.../ and //
+result=17/2 #...for the performance of floating point operations
+print(result)
+
+result=17//2 #...for the performance of integer point operations
+print(result)
+
+# % operator
+result=17%2
+print(result)  #...for calculating the remainder point operations
